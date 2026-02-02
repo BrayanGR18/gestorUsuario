835436cb6ffb07c9832116326756f954609a400e
Brayan <brayan.gtzr02@gmail.com>
2026-02-02 14:43:14 -0600

    Subiendo el proyecto

---
+++ src/test/java/com/pruebaTecnica/gestorUsuario/GestorUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.gestorUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
